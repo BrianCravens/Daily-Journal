4d1e55a9fae4cd5f4e688f185542b0996252e403
Brian Cravens <brian.cravens303@gmail.com>
2020-04-17 16:19:20 -0500

    DailyJournal2

---
+++ src/scripts/journal.js
@@ -0,0 +1,10 @@
+let journalEntry = {
+date: "4/17/20",
+title: "Objects and Arrays",
+contents: "Today we went over Objects and Arrays, and how to call/replace information from them. ",
+mood: "Excited"
+}
+
+let journals = []
+journals.push(journalEntry)
+console.log(journals)

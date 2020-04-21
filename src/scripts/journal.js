bfef6a467b122c841ad8139d41ae44bf6a95ab6b
Brian Cravens <brian.cravens303@gmail.com>
2020-04-21 14:47:17 -0500

    Updated Journal

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,10 +1,32 @@
-let journalEntry = {
-date: "4/17/20",
-title: "Objects and Arrays",
-contents: "Today we went over Objects and Arrays, and how to call/replace information from them. ",
-mood: "Excited"
-}
+const journalEntry1 = {
+  date: "4/17/20",
+  title: "Objects and Arrays",
+  contents:
+    "Today we went over Objects and Arrays, and how to call/replace information from them. ",
+  mood: "Excited",
+};
+const journalEntry2 = {
+  date: "4/17/20",
+  title: "Objects and Arrays",
+  contents:
+    "Today we went over Objects and Arrays, and how to call/replace information from them. ",
+  mood: "Excited",
+};
+const journalEntry3 = {
+  date: "4/17/20",
+  title: "Objects and Arrays",
+  contents:
+    "Today we went over Objects and Arrays, and how to call/replace information from them. ",
+  mood: "Excited",
+};
+const journalEntry4 = {
+  date: "4/17/20",
+  title: "Objects and Arrays",
+  contents:
+    "Today we went over Objects and Arrays, and how to call/replace information from them. ",
+  mood: "Excited",
+};
 
-let journals = []
-journals.push(journalEntry)
-console.log(journals)
+let journalEntries = [];
+journalEntries.push(journalEntry1,journalEntry2,journalEntry3,journalEntry4);
+console.log(journalEntries);
